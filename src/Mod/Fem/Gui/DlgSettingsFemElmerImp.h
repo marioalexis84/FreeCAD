d9cf4124471f066c340239e89c0c1772db3a0623
marioalexis <mario.passaglia@gmail.com>
2025-09-06 17:28:50 -0300

    Fem: Fix Elmer preferences file chooser

--- src/Mod/Fem/Gui/DlgSettingsFemElmerImp.h
+++ src/Mod/Fem/Gui/DlgSettingsFemElmerImp.h
@@ -41,9 +41,7 @@ public:
     ~DlgSettingsFemElmerImp() override;
 
 protected Q_SLOTS:
-    void onfileNameChanged(QString FileName);
-    void onfileNameChangedMT(QString FileName);
-    void onCoresValueChanged(int cores);
+    void onfileNameSelected(QString FileName);
 
 protected:
     void saveSettings() override;
