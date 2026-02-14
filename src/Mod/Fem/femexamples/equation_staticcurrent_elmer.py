c09ecf5b565ea6b06603ef3fb682bb2b4bf29e33
marioalexis <mario.passaglia@gmail.com>
2026-02-14 01:26:15 -0300

    Fem: Rename initialTemperature to InitialTemperature

--- src/Mod/Fem/femexamples/equation_staticcurrent_elmer.py
+++ src/Mod/Fem/femexamples/equation_staticcurrent_elmer.py
@@ -148,7 +148,7 @@ def setup(doc=None, solvertype="elmer"):
 
     # constraint initial temperature
     con_inittemp = ObjectsFem.makeConstraintInitialTemperature(doc, "ConstraintInitialTemperature")
-    con_inittemp.initialTemperature = 300.0
+    con_inittemp.InitialTemperature = 300.0
     analysis.addObject(con_inittemp)
 
     # mesh
